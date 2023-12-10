4f195af17bba2491bd514f19d46e0740a0caf051
smriti0302 <smriti030202@gmail.com>
2023-12-10 17:49:17 +0530

    modified login func

--- backend/init.sequelize.js
+++ backend/init.sequelize.js
@@ -1,19 +1,18 @@
-const { Sequelize } = require('sequelize');
-const SQLite = require('sqlite3');
+const { Sequelize } = require("sequelize");
+const SQLite = require("sqlite3");
 
-var sequelize = new Sequelize('yoga', 'root', 'root', {
-    dialect: 'sqlite',
-    storage: './app.db',
-    dialectOptions: {
-        mode:
-            SQLite.OPEN_READWRITE | SQLite.OPEN_CREATE | SQLite.OPEN_FULLMUTEX,
-    },
-    logging: false,
+var sequelize = new Sequelize("yoga", "root", "root", {
+  dialect: "sqlite",
+  storage: "./app.db",
+  dialectOptions: {
+    mode: SQLite.OPEN_READWRITE | SQLite.OPEN_CREATE | SQLite.OPEN_FULLMUTEX,
+  },
+  logging: false,
 });
 
 // Initializes and syncs the db
 function initializeSequelize() {
-    return sequelize.sync({ alter: true });
+  return sequelize.sync();
 }
 
 module.exports = { sequelize, initializeSequelize };
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,281 +1,281 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const auth = require('../oauth');
-const { User: UserSQL } = require('../models/sql/User');
-const brypt = require('bcrypt');
+const auth = require("../oauth");
+const { User: UserSQL } = require("../models/sql/User");
+const brypt = require("bcrypt");
 const {
-    HTTP_BAD_REQUEST,
-    HTTP_OK,
-    HTTP_INTERNAL_SERVER_ERROR,
-} = require('../utils/http_status_codes');
-const { Institute } = require('../models/sql/Institute');
-const { Role } = require('../models/sql/Role');
-const { Op } = require('sequelize');
-const { sequelize } = require('../init.sequelize');
-const { timeout } = require('../utils/promise_timeout');
-const { validate_email } = require('../utils/validate_email');
+  HTTP_BAD_REQUEST,
+  HTTP_OK,
+  HTTP_INTERNAL_SERVER_ERROR,
+} = require("../utils/http_status_codes");
+const { Institute } = require("../models/sql/Institute");
+const { Role } = require("../models/sql/Role");
+const { Op } = require("sequelize");
+const { sequelize } = require("../init.sequelize");
+const { timeout } = require("../utils/promise_timeout");
+const { validate_email } = require("../utils/validate_email");
 
-router.post('/auth/verify-google', async (req, res) => {
-    // console.log(req.body);
-    const { client_id, jwtToken } = req.body;
-    try {
-        // console.log('trying to verify');
-        const userInfo = await auth.verify(client_id, jwtToken);
-        // console.log('verified');
-        // console.log(userInfo);
-        return res.status(HTTP_OK).json(userInfo);
-    } catch (error) {
-        console.error('Authentication error:', error.message);
-        return res.status(401).json({ error: 'Authentication failed' });
-    }
+router.post("/auth/verify-google", async (req, res) => {
+  // console.log(req.body);
+  const { client_id, jwtToken } = req.body;
+  try {
+    // console.log('trying to verify');
+    const userInfo = await auth.verify(client_id, jwtToken);
+    // console.log('verified');
+    // console.log(userInfo);
+    return res.status(HTTP_OK).json(userInfo);
+  } catch (error) {
+    console.error("Authentication error:", error.message);
+    return res.status(401).json({ error: "Authentication failed" });
+  }
 });
 
-router.post('/auth/login', async (req, res) => {
-    // TODO : jwt token
-    const { username, password } = req.body;
+router.post("/auth/login", async (req, res) => {
+  // TODO : jwt token
+  const { username, password } = req.body;
 
-    if (!username || !password)
-        res.status(HTTP_BAD_REQUEST).json({ error: 'Missing required fields' });
+  if (!username || !password)
+    return res
+      .status(HTTP_BAD_REQUEST)
+      .json({ error: "Missing required fields" });
 
-    // check if user exists
-    const user = await UserSQL.findOne({
-        where: { username: username },
-        attributes: ['id', 'name', 'username', 'email', 'password'],
-        include: [
-            { model: Institute, attributes: ['name'] },
-            { model: Role, attributes: ['name'] },
-        ],
+  // check if user exists
+  const user = await UserSQL.findOne({
+    where: { username: username },
+    attributes: ["id", "name", "username", "email", "password"],
+    include: [
+      { model: Institute, attributes: ["name"] },
+      { model: Role, attributes: ["name"] },
+    ],
+  });
+
+  if (!user)
+    return res.status(HTTP_BAD_REQUEST).json({ error: "User does not exist" });
+
+  // check password
+  const validPassword = await brypt.compare(password, user.password);
+
+  if (!validPassword)
+    return res.status(HTTP_BAD_REQUEST).json({ error: "Invalid password" });
+
+  return res.status(HTTP_OK).json({ user });
+});
+
+router.post("/auth/register", async (req, res) => {
+  const {
+    username,
+    password,
+    confirm_password,
+    email,
+    // phone_number,
+    name,
+    institute_name,
+    role_name,
+    is_google_login,
+  } = req.body;
+  console.log("HI");
+  console.log(email);
+  // validate inputs
+  if (
+    !username ||
+    !password ||
+    !email ||
+    !name ||
+    !role_name ||
+    is_google_login === undefined ||
+    is_google_login === null
+  )
+    return res
+      .status(HTTP_BAD_REQUEST)
+      .json({ error: "Missing required fields" });
+
+  if (!validate_email(email)) {
+    return res.status(HTTP_BAD_REQUEST).json({ error: "Invalid email" });
+  }
+
+  // check password
+  if (password !== confirm_password)
+    return res
+      .status(HTTP_BAD_REQUEST)
+      .json({ error: "Passwords do not match" });
+
+  if (password.length < 4) {
+    return res.status(HTTP_BAD_REQUEST).json({
+      error: "Password must be at least 4 characters long",
     });
+  }
 
-    if (!user)
-        res.status(HTTP_BAD_REQUEST).json({ error: 'User does not exist' });
+  // check if user exists
+  const user = await UserSQL.findOne({
+    where: {
+      [Op.or]: [{ username: username }, { email: email }],
+    },
+    attributes: ["id", "name", "username", "email"],
+  });
 
-    // check password
-    const validPassword = await brypt.compare(password, user.password);
+  // console.log(user);
 
-    if (!validPassword)
-        res.status(HTTP_BAD_REQUEST).json({ error: 'Invalid password' });
+  if (user && user.username === username)
+    return res
+      .status(HTTP_BAD_REQUEST)
+      .json({ error: "Username already exists" });
 
-    res.status(HTTP_OK).json({ user });
-});
+  if (user && user.email === email)
+    return res.status(HTTP_BAD_REQUEST).json({ error: "Email already exists" });
 
-router.post('/auth/register', async (req, res) => {
-    const {
+  // hash password
+  const salt = await brypt.genSalt(10);
+  const hashedPassword = await brypt.hash(password, salt);
+
+  // db transaction
+  const t = await sequelize.transaction();
+  try {
+    // find insitite by name
+    let institute = null;
+
+    if (institute_name !== "") {
+      institute = await Institute.findOne(
+        {
+          where: { name: institute_name },
+          attributes: ["id"],
+        },
+        { transaction: t }
+      );
+
+      if (!institute) throw new Error("Institute doesn't exist");
+    }
+
+    // find role by name
+    const role = await Role.findOne(
+      {
+        where: { name: role_name },
+        attributes: ["id"],
+      },
+      { transaction: t }
+    );
+
+    if (!role) throw new Error("Role doesn't exist");
+
+    // create user
+    const newUser = await UserSQL.create(
+      {
         username,
-        password,
-        confirm_password,
-        email,
+        password: hashedPassword,
         name,
-        institute_name,
-        role_name,
+        email,
+        institute_id: institute ? institute.id : null,
+        role_id: role.id,
         is_google_login,
-    } = req.body;
+      },
+      { transaction: t }
+    );
 
-    // validate inputs
-    if (
-        !username ||
-        !password ||
-        !email ||
-        !name ||
-        !role_name ||
-        is_google_login === undefined ||
-        is_google_login === null
-    )
+    // console.log('committing');
+    await timeout(t.commit(), 5000, new Error("timeout; try again"));
+
+    res.status(HTTP_OK).json({ user: newUser });
+  } catch (error) {
+    console.error(error);
+    await t.rollback();
+
+    switch (error.message) {
+      case "Institute doesn't exist":
+      case "Role doesn't exist":
         return res
-            .status(HTTP_BAD_REQUEST)
-            .json({ error: 'Missing required fields' });
-
-    if (!validate_email(email)) {
-        return res.status(HTTP_BAD_REQUEST).json({ error: 'Invalid email' });
-    }
-
-    // check password
-    if (password !== confirm_password)
-        return res
-            .status(HTTP_BAD_REQUEST)
-            .json({ error: 'Passwords do not match' });
-
-    if (password.length < 4) {
-        return res.status(HTTP_BAD_REQUEST).json({
-            error: 'Password must be at least 4 characters long',
+          .status(HTTP_BAD_REQUEST)
+          .json({ error: "Role doesn't exist" });
+      default:
+        return res.status(HTTP_INTERNAL_SERVER_ERROR).json({
+          error: error.message,
         });
     }
-
-    // check if user exists
-    const user = await UserSQL.findOne({
-        where: {
-            [Op.or]: [{ username: username }, { email: email }],
-        },
-        attributes: ['id', 'name', 'username', 'email'],
-    });
-
-    // console.log(user);
-
-    if (user && user.username === username)
-        return res
-            .status(HTTP_BAD_REQUEST)
-            .json({ error: 'Username already exists' });
-
-    if (user && user.email === email)
-        return res
-            .status(HTTP_BAD_REQUEST)
-            .json({ error: 'Email already exists' });
-
-    // hash password
-    const salt = await brypt.genSalt(10);
-    const hashedPassword = await brypt.hash(password, salt);
-
-    // db transaction
-    const t = await sequelize.transaction();
-    try {
-        // find insitite by name
-        let institute = null;
-
-        if (institute_name !== '') {
-            institute = await Institute.findOne(
-                {
-                    where: { name: institute_name },
-                    attributes: ['id'],
-                },
-                { transaction: t }
-            );
-
-            if (!institute) throw new Error("Institute doesn't exist");
-        }
-
-        // find role by name
-        const role = await Role.findOne(
-            {
-                where: { name: role_name },
-                attributes: ['id'],
-            },
-            { transaction: t }
-        );
-
-        if (!role) throw new Error("Role doesn't exist");
-
-        // create user
-        const newUser = await UserSQL.create(
-            {
-                username,
-                password: hashedPassword,
-                name,
-                email,
-                institute_id: institute ? institute.id : null,
-                role_id: role.id,
-                is_google_login,
-            },
-            { transaction: t }
-        );
-
-        // console.log('committing');
-        await timeout(t.commit(), 5000, new Error('timeout; try again'));
-
-        res.status(HTTP_OK).json({ user: newUser });
-    } catch (error) {
-        console.error(error);
-        await t.rollback();
-
-        switch (error.message) {
-            case "Institute doesn't exist":
-            case "Role doesn't exist":
-                return res
-                    .status(HTTP_BAD_REQUEST)
-                    .json({ error: "Role doesn't exist" });
-            default:
-                return res.status(HTTP_INTERNAL_SERVER_ERROR).json({
-                    error: error.message,
-                });
-        }
-    }
+  }
 });
 
-router.post('/auth/register-google', async (req, res) => {
-    // used to register user whos logging in using google oauth
-    const { email, name, is_google_login, client_id, jwt_token } = req.body;
+router.post("/auth/register-google", async (req, res) => {
+  // used to register user whos logging in using google oauth
+  const { email, name, is_google_login, client_id, jwt_token } = req.body;
 
-    // validate inputs
-    if (
-        !email ||
-        !name ||
-        !jwt_token ||
-        !client_id ||
-        is_google_login === undefined ||
-        is_google_login === null
-    )
-        return res
-            .status(HTTP_BAD_REQUEST)
-            .json({ error: 'Missing required fields' });
+  // validate inputs
+  if (
+    !email ||
+    !name ||
+    !jwt_token ||
+    !client_id ||
+    is_google_login === undefined ||
+    is_google_login === null
+  )
+    return res
+      .status(HTTP_BAD_REQUEST)
+      .json({ error: "Missing required fields" });
 
-    const userInfo = await auth.verify(client_id, jwt_token);
+  const userInfo = await auth.verify(client_id, jwt_token);
 
-    if (!userInfo) {
-        return res
-            .status(HTTP_BAD_REQUEST)
-            .json({ error: 'Invalid Google OAuth token' });
-    }
+  if (!userInfo) {
+    return res
+      .status(HTTP_BAD_REQUEST)
+      .json({ error: "Invalid Google OAuth token" });
+  }
 
-    if (userInfo.email !== email) {
-        return res.status(HTTP_BAD_REQUEST).json({
-            error: 'unmatched email',
-        });
-    }
-
-    if (!validate_email(email)) {
-        return res.status(HTTP_BAD_REQUEST).json({ error: 'Invalid email' });
-    }
-
-    if (!is_google_login) {
-        return res.status(HTTP_BAD_REQUEST).json({
-            error: 'user not registered using Google OAuth',
-        });
-    }
-
-    // check if user exists
-    const user = await UserSQL.findOne({
-        where: {
-            email: email,
-        },
-        attributes: ['id', 'name', 'username', 'email'],
+  if (userInfo.email !== email) {
+    return res.status(HTTP_BAD_REQUEST).json({
+      error: "unmatched email",
     });
+  }
 
-    // console.log(user);
+  if (!validate_email(email)) {
+    return res.status(HTTP_BAD_REQUEST).json({ error: "Invalid email" });
+  }
 
-    if (user && user.email === email)
-        return res
-            .status(HTTP_BAD_REQUEST)
-            .json({ error: 'Email already exists' });
+  if (!is_google_login) {
+    return res.status(HTTP_BAD_REQUEST).json({
+      error: "user not registered using Google OAuth",
+    });
+  }
 
-    // db transaction
-    const t = await sequelize.transaction();
+  // check if user exists
+  const user = await UserSQL.findOne({
+    where: {
+      email: email,
+    },
+    attributes: ["id", "name", "username", "email"],
+  });
 
-    try {
-        // create user
-        const newUser = await UserSQL.create(
-            {
-                name,
-                email,
-                is_google_login,
-            },
-            { transaction: t }
-        );
+  // console.log(user);
 
-        // console.log('committing');
-        await timeout(t.commit(), 5000, new Error('timeout; try again'));
+  if (user && user.email === email)
+    return res.status(HTTP_BAD_REQUEST).json({ error: "Email already exists" });
 
-        res.status(HTTP_OK).json({ user: newUser });
-    } catch (error) {
-        console.error(error);
-        await t.rollback();
+  // db transaction
+  const t = await sequelize.transaction();
 
-        switch (error.message) {
-            default:
-                return res.status(HTTP_INTERNAL_SERVER_ERROR).json({
-                    error: error.message,
-                });
-        }
+  try {
+    // create user
+    const newUser = await UserSQL.create(
+      {
+        name,
+        email,
+        is_google_login,
+      },
+      { transaction: t }
+    );
+
+    // console.log('committing');
+    await timeout(t.commit(), 5000, new Error("timeout; try again"));
+
+    res.status(HTTP_OK).json({ user: newUser });
+  } catch (error) {
+    console.error(error);
+    await t.rollback();
+
+    switch (error.message) {
+      default:
+        return res.status(HTTP_INTERNAL_SERVER_ERROR).json({
+          error: error.message,
+        });
     }
+  }
 });
 
 module.exports = router;
