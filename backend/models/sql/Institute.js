c723943435ae22a179d01b2916fe9a03d6f6aab4
smriti0302 <smriti030202@gmail.com>
2023-12-26 09:58:11 +0530

    dropdown

--- backend/models/sql/Institute.js
+++ backend/models/sql/Institute.js
@@ -1,41 +1,61 @@
-const { sequelize } = require("../../init.sequelize");
-const { DataTypes } = require("sequelize");
-const { options } = require("./defaultOptions");
+const { sequelize } = require('../../init.sequelize');
+const { DataTypes } = require('sequelize');
+const { options } = require('./defaultOptions');
 
 const Institute = sequelize.define(
-  "institute",
-  {
-    institute_id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+    'institute',
+    {
+        institute_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address1: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        address2: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        country: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        pincode: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        billing_address: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        gstin: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    address1: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    address2: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    billing_address: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  },
-  { ...options }
+    { ...options }
 );
 
 module.exports = { Institute };
