66dd1682bef90e0072506e6810de570de52b5c1c
smriti0302 <smriti030202@gmail.com>
2024-01-07 11:09:15 +0530

    updates

--- frontend/src/pages/institute/settings/AddNewTeacher.js
+++ frontend/src/pages/institute/settings/AddNewTeacher.js
@@ -1,37 +1,298 @@
-import { Tabs, Input, Button } from "@geist-ui/core";
-import InstituteNavbar from "../../../components/Common/InstituteNavbar/InstituteNavbar";
+import { Button, Input, Table, Tag } from "@geist-ui/core";
+import { Copy } from "@geist-ui/icons";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import InstitutePageWrapper from "../../../components/Common/InstitutePageWrapper";
+import useUserStore from "../../../store/UserStore";
+import { Fetch } from "../../../utils/Fetch";
+import { validateEmail, validatePhone } from "../../../utils/formValidation";
+import getFormData from "../../../utils/getFormData";
 
 export default function AddNewTeacher() {
+  const user = useUserStore((state) => state.user);
+  const [invites, setInvites] = useState([]);
+  const [addLoading, setAddLoading] = useState(false);
+  const [refreshLoading, setRefreshLoading] = useState(false);
+
+  const getInvites = useCallback(async () => {
+    setRefreshLoading(true);
+    Fetch({
+      url: "http://localhost:4000/invite/get-all-by-inviterid",
+      method: "POST",
+      data: {
+        inviter_user_id: user.user_id,
+      },
+    })
+      .then((res) => {
+        setInvites(res.data.invites);
+        toast("Invites fetched successfully", { type: "success" });
+        setRefreshLoading(false);
+      })
+      .catch((err) => {
+        toast(`Error : ${err?.response?.data?.message}`, {
+          type: "error",
+        });
+        setRefreshLoading(false);
+      });
+  }, [user.user_id]);
+
+  useEffect(() => {
+    getInvites();
+  }, [getInvites]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setAddLoading(true);
+
+    const formData = getFormData(e);
+
+    if (!formData) return;
+
+    if (!formData.name) {
+      setAddLoading(false);
+      toast("Teacher name is required", { type: "error" });
+      return;
+    }
+
+    const [validEmail, emailError] = validateEmail(formData.email);
+
+    if (!validEmail) {
+      toast(emailError.message, { type: "error" });
+      setAddLoading(false);
+      return;
+    }
+
+    const [validPhone, phoneError] = validatePhone(formData.phone);
+
+    if (!validPhone) {
+      toast(phoneError.message, { type: "error" });
+      setAddLoading(false);
+      return;
+    }
+
+    formData.invite_type = "TEACHER";
+    formData.user_id = user.user_id;
+
+    // console.log(formData);
+
+    Fetch({
+      url: "http://localhost:4000/invite/create",
+      method: "POST",
+      data: formData,
+    })
+      .then((res) => {
+        toast("Teacher added successfully", { type: "success" });
+        e.target.reset();
+        getInvites();
+        setAddLoading(false);
+      })
+      .catch((err) => {
+        toast(`Error : ${err?.response?.data?.message}`, {
+          type: "error",
+        });
+        setAddLoading(false);
+      });
   };
-  return (
-    <div>
-      <div>
-        <InstituteNavbar />
+
+  const retractInvite = (setRetractLoading, invite_id, invite_token) => {
+    setRetractLoading(true);
+    Fetch({
+      url: "http://localhost:4000/invite/retract",
+      method: "POST",
+      data: {
+        invite_id: invite_id,
+        invite_token: invite_token,
+      },
+    })
+      .then((res) => {
+        toast("Invite retracted successfully", {
+          type: "success",
+        });
+        getInvites();
+        setRetractLoading(false);
+      })
+      .catch((err) => {
+        toast(`Error : ${err?.response?.data?.message}`, {
+          type: "error",
+        });
+        setRetractLoading(false);
+      });
+  };
+
+  const resendInvite = (setResendLoading, invite_id) => {
+    setResendLoading(true);
+    Fetch({
+      url: "http://localhost:4000/invite/resend",
+      method: "POST",
+      data: {
+        invite_id: invite_id,
+      },
+    })
+      .then((res) => {
+        toast("Invite sent successfully", {
+          type: "success",
+        });
+        getInvites();
+        setResendLoading(false);
+      })
+      .catch((err) => {
+        toast(`Error : ${err?.response?.data?.message}`, {
+          type: "error",
+        });
+        setResendLoading(false);
+      });
+  };
+
+  const copyToClipboard = (token) => {
+    try {
+      navigator.clipboard.writeText(
+        `http://localhost:3000/teacher/invite?token=${token}`
+      );
+      toast("Copied to clipboard", {
+        type: "success",
+      });
+    } catch (err) {
+      toast("Error copying to clipboard", {
+        type: "error",
+      });
+    }
+  };
+
+  const InviteActions = ({ row }) => {
+    const [resendLoading, setResendLoading] = useState(false);
+    const [retractLoading, setRetractLoading] = useState(false);
+
+    return (
+      <div className="flex gap-2">
+        <Button
+          icon={<Copy />}
+          scale={0.7}
+          w={0.5}
+          disabled={row.is_retracted || row.is_filled}
+          onClick={() => copyToClipboard(row.token)}
+        ></Button>
+        <Button
+          type="secondary"
+          scale={0.7}
+          w={0.5}
+          disabled={row.is_retracted || row.is_accepted || row.is_filled}
+          loading={resendLoading}
+          onClick={() => {
+            resendInvite(setResendLoading, row.invite_id);
+          }}
+        >
+          Resend
+        </Button>
+        <Button
+          type="error-light"
+          scale={0.7}
+          w={0.5}
+          disabled={row.is_retracted || row.is_filled}
+          loading={retractLoading}
+          onClick={() =>
+            retractInvite(setRetractLoading, row.invite_id, row.token)
+          }
+        >
+          Retract
+        </Button>
       </div>
-      <div className="max-w-5xl mx-auto">
+    );
+  };
+
+  const renderInviteStatus = (value, row, index) => {
+    return (
+      <>
+        {row.is_retracted ? (
+          <Tag type="error">Retracted</Tag>
+        ) : row.is_filled ? (
+          <Tag type="success">Registered</Tag>
+        ) : row.is_accepted ? (
+          <Tag type="success">Accepted</Tag>
+        ) : (
+          <Tag type="warning">Pending</Tag>
+        )}
+      </>
+    );
+  };
+
+  const renderExpiryStatus = (value, row, index) => {
+    const diff = (new Date(row.expiry_date).getTime() - Date.now()) / 1000;
+    return (
+      <>
+        <p>
+          <span>
+            {diff > 86400
+              ? (diff / 86400).toFixed(2) + " Days"
+              : diff > 0
+              ? (diff / 3600).toFixed(2) + " Hours"
+              : "Expired"}
+          </span>
+          <br />
+          <span className="text-sm text-zinc-400">
+            {new Date(row.expiry_date).toDateString()}
+          </span>
+        </p>
+      </>
+    );
+  };
+
+  return (
+    <InstitutePageWrapper heading="Teacher Invite Management">
+      <div className="card-base">
+        <h3>Add a teacher</h3>
         <form className="flex flex-col gap-2 my-8" onSubmit={handleSubmit}>
-          <Input width="100%" placeholder="John Doe" name="teacher_name">
+          <Input width="100%" placeholder="John Doe" name="name" required>
             Teacher Name
           </Input>
           <Input
             width="100%"
             placeholder="teacher@gmail.com"
-            name="teacher_email"
+            name="email"
+            required
           >
             Teacher Email ID
           </Input>
-          <Input width="100%" placeholder="9876543210" name="teacher_phone">
+          <Input width="100%" placeholder="9876543210" name="phone" required>
             Teacher Phone Number
           </Input>
           <div className="flex flex-row gap-2 w-full">
-            <Button className="flex-1" type="secondary" htmlType="submit">
-              Update
+            <Button
+              className="flex-1"
+              type="secondary"
+              htmlType="submit"
+              loading={addLoading}
+            >
+              Add
             </Button>
           </div>
         </form>
       </div>
-    </div>
+
+      <div className="my-20">
+        <div className="flex justify-between items-center">
+          <h2>Invites</h2>
+          <Button onClick={getInvites} loading={refreshLoading}>
+            Refresh
+          </Button>
+        </div>
+        <Table data={invites} emptyText="---">
+          <Table.Column prop="invite_id" label="ID" />
+          <Table.Column prop="email" label="Email" />
+          <Table.Column prop="invite_type" label="Invite Type" />
+          <Table.Column
+            prop="status"
+            label="Status"
+            render={renderInviteStatus}
+          />
+          <Table.Column label="Expiry" render={renderExpiryStatus} />
+          <Table.Column
+            label="Actions"
+            render={(_, row, index) => {
+              return <InviteActions row={row} key={index} />;
+            }}
+          />
+        </Table>
+      </div>
+    </InstitutePageWrapper>
   );
 }
--- frontend/src/pages/teacher/invite/Index.js
+++ frontend/src/pages/teacher/invite/Index.js
@@ -1,164 +1,281 @@
-import { Button, Input, Tag } from '@geist-ui/core';
-import { useState } from 'react';
-import getFormData from '../../../utils/getFormData';
+import { Button, Input, Tag } from "@geist-ui/core";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Fetch } from "../../../utils/Fetch";
+import getFormData from "../../../utils/getFormData";
 
 function Card({ title, description }) {
-    return (
-        <div className='border rounded-lg p-4'>
-            <h1 className='text-center'>{title}</h1>
-            <p className='text-center'>{description}</p>
+  return (
+    <div className="border rounded-lg p-4">
+      <h1 className="text-center">{title}</h1>
+      <p className="text-center">{description}</p>
+    </div>
+  );
+}
+
+function VerifyEmailCard({
+  inviteToken,
+  setConfirmEmail,
+  handleAccept,
+  handleReject,
+  instituteName,
+  expiryDiffSeconds,
+}) {
+  return (
+    <div className="border rounded-lg p-4">
+      <h1 className="text-center">Invite</h1>
+      <div className="text-sm text-center">
+        <div className="mb-4">
+          <p>To join the institute : {instituteName}</p>
+          <p>
+            Invite Expires in :{" "}
+            {expiryDiffSeconds < 86400
+              ? `${(expiryDiffSeconds / 3600)?.toFixed(3)} Hours`
+              : `${(expiryDiffSeconds / 86400)?.toFixed(3)} Days`}
+          </p>
         </div>
-    );
+      </div>
+
+      <Input
+        name="confirm_email"
+        placeholder="Enter Email to verify"
+        w="100%"
+        className="mb-4"
+        onChange={(e) => setConfirmEmail(e.target.value)}
+      />
+      <div className="flex gap-2">
+        <Button type="success" onClick={handleAccept}>
+          Accept
+        </Button>
+        <Button type="primary" onClick={handleReject}>
+          Reject
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+function UpdateDetailsCard({
+  inviteToken,
+  handleUpdateDetails,
+  instituteName,
+}) {
+  return (
+    <div className="border rounded-lg p-4">
+      <h1 className="text-center">Invite</h1>
+      <div className="text-sm text-center">
+        {/* <p>Invite Token : {inviteToken}</p> */}
+        <p>To join the institute : {instituteName}</p>
+        <div className="mt-4">
+          <Tag type="success" w="100%">
+            Teacher Verified
+          </Tag>
+        </div>
+      </div>
+
+      <form className="flex flex-col gap-4 mt-4" onSubmit={handleUpdateDetails}>
+        <p className="text-center font-bold">Update your login details!</p>
+        <label for="username">
+          Username
+          <Input name="username" w="100%" />
+        </label>
+        <label htmlFor="new_password">
+          Password
+          <Input.Password name="new_password" w="100%" />
+        </label>
+        <label htmlFor="confirm_new_password">
+          Confirm Password
+          <Input.Password name="confirm_new_password" w="100%" />
+        </label>
+        <Button type="success" htmlType="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default function InvitePage() {
-    const [accepted, setAccepted] = useState(false);
-    const [verified, setVerified] = useState(false);
-    const [used, setUsed] = useState(false);
-    const [expired, setExpired] = useState(false);
-    const [confirmEmail, setConfirmEmail] = useState('');
+  const [token, setToken] = useState(null);
+  const [invite, setInvite] = useState(null);
+  const [accepted, setAccepted] = useState(false);
+  const [verified, setVerified] = useState(false);
+  const [expired, setExpired] = useState(false);
+  const [used, setUsed] = useState(false);
+  const [confirmEmail, setConfirmEmail] = useState("");
 
-    const handleAccept = (e) => {
-        e.preventDefault();
-        setVerified(true);
-    };
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    const handleReject = (e) => {
-        e.preventDefault();
-    };
+  useEffect(() => {
+    setToken(() => {
+      if (location.search.includes("?token=")) {
+        return location.search.split("?token=")[1];
+      } else {
+        return null;
+      }
+    });
+  }, [location]);
 
-    const handleUpdateDetails = (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    if (token) {
+      Fetch({
+        url: "http://localhost:4000/invite/get-by-token",
+        method: "POST",
+        data: {
+          token: token,
+        },
+      }).then((res) => {
+        // console.log(res.data);
+        if (res.status === 200) {
+          setInvite(res.data.invite);
 
-        const formData = getFormData(e);
+          if (res.data.invite.is_filled) {
+            setUsed(true);
+          }
 
-        console.log(formData);
-    };
+          if (new Date(res.data.invite.expiry_date).getTime() < Date.now()) {
+            setExpired(true);
+          }
 
-    return (
-        <div className='grid place-content-center min-h-screen'>
-            {!expired ? (
-                <>
-                    {!accepted ? (
-                        <>
-                            {!used ? (
-                                <>
-                                    {verified ? (
-                                        <div className='border rounded-lg p-4'>
-                                            <h1 className='text-center'>
-                                                Invite
-                                            </h1>
-                                            <div className='text-sm text-center'>
-                                                <p>
-                                                    To join the institute :
-                                                    XYZABCD123456789101112
-                                                </p>
-                                                <p>
-                                                    Invite Expires in : {6} Days
-                                                </p>
-                                                <div className='mt-4'>
-                                                    <Tag
-                                                        type='success'
-                                                        w='100%'>
-                                                        Teacher Verified
-                                                    </Tag>
-                                                </div>
-                                            </div>
+          if (res.data.invite.is_accepted) {
+            setAccepted(true);
+          }
+        }
+      });
+    }
+  }, [token]);
 
-                                            <form
-                                                className='flex flex-col gap-4 mt-4'
-                                                onSubmit={handleUpdateDetails}>
-                                                <p className='text-center font-bold'>
-                                                    Update your login details!
-                                                </p>
-                                                <label for='username'>
-                                                    Username
-                                                    <Input
-                                                        name='username'
-                                                        w='100%'
-                                                    />
-                                                </label>
-                                                <label for='password'>
-                                                    Password
-                                                    <Input.Password
-                                                        name='password'
-                                                        w='100%'
-                                                    />
-                                                </label>
-                                                <label for='confirm_password'>
-                                                    Confirm Password
-                                                    <Input.Password
-                                                        name='confirm_password'
-                                                        w='100%'
-                                                    />
-                                                </label>
-                                                <Button
-                                                    type='success'
-                                                    htmlType='submit'>
-                                                    Submit
-                                                </Button>
-                                            </form>
-                                        </div>
-                                    ) : (
-                                        <div className='border rounded-lg p-4'>
-                                            <h1 className='text-center'>
-                                                Invite
-                                            </h1>
-                                            <div className='text-sm text-center'>
-                                                <p>
-                                                    To join the institute :
-                                                    XYZABCD123456789101112
-                                                </p>
-                                                <p>
-                                                    Invite Expires in : {6} Days
-                                                </p>
-                                            </div>
+  const handleAccept = (e) => {
+    e.preventDefault();
 
-                                            <Input
-                                                name='confirm_email'
-                                                placeholder='Enter Email to verify'
-                                                w='100%'
-                                                onChange={(val) =>
-                                                    setConfirmEmail(val)
-                                                }
-                                            />
-                                            <div className='flex gap-2'>
-                                                <Button
-                                                    type='success'
-                                                    onClick={handleAccept}>
-                                                    Accept
-                                                </Button>
-                                                <Button
-                                                    type='primary'
-                                                    onClick={handleReject}>
-                                                    Reject
-                                                </Button>
-                                            </div>
-                                        </div>
-                                    )}
-                                </>
-                            ) : (
-                                <Card
-                                    title='You have declined the invitation'
-                                    description='You can no longer create your account; Contact the institute owner'
-                                />
-                            )}
-                        </>
-                    ) : (
-                        <Card
-                            title='You have accepted the invitation'
-                            description='You can now create your account'
-                        />
-                    )}
-                </>
-            ) : (
-                <>
-                    <Card
-                        title='Invite Expired'
-                        description='You can no longer create your account; Contact the institute owner'
-                    />
-                </>
-            )}
-        </div>
-    );
+    Fetch({
+      url: "http://localhost:4000/invite/accept",
+      method: "POST",
+      data: {
+        invite_token: token,
+        confirm_email: confirmEmail,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setVerified(true);
+          toast("Email Verified", { type: "success" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        toast(err?.response?.data?.message, { type: "error" });
+      });
+  };
+
+  const handleReject = (e) => {
+    e.preventDefault();
+
+    Fetch({
+      url: "http://localhost:4000/invite/reject",
+      method: "POST",
+      data: {
+        invite_token: token,
+        confirm_email: confirmEmail,
+      },
+    })
+      .then((res) => {
+        // console.log(res);
+        if (res.status === 200) {
+          setUsed(true);
+          setAccepted(false);
+          toast("Invite Rejected", { type: "success" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        toast(err?.response?.data?.message, { type: "error" });
+      });
+  };
+
+  const handleUpdateDetails = (e) => {
+    e.preventDefault();
+
+    const formData = getFormData(e);
+    console.log(formData);
+
+    Fetch({
+      url: "http://localhost:4000/invite/update-userdetails",
+      method: "POST",
+      data: {
+        invite_token: token,
+        ...formData,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setAccepted(true);
+          toast("Details Updated", { type: "success" });
+          navigate("/auth");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        toast(err?.response?.data?.message, { type: "error" });
+      });
+  };
+
+  const notUsedComponent = (
+    <>
+      {verified ? (
+        // !expired !accepted !used verified
+        <UpdateDetailsCard
+          handleUpdateDetails={handleUpdateDetails}
+          inviteToken={token}
+          instituteName={invite?.institute_name}
+        />
+      ) : (
+        // !expired !accepted !used !verified
+        <VerifyEmailCard
+          inviteToken={token}
+          setConfirmEmail={setConfirmEmail}
+          handleAccept={handleAccept}
+          handleReject={handleReject}
+          instituteName={invite?.institute_name}
+          expiryDiffSeconds={
+            (new Date(invite?.expiry_date).getTime() - Date.now()) / 1000
+          }
+        />
+      )}
+    </>
+  );
+
+  const acceptedComponent = (
+    <>
+      {!accepted ? (
+        <Card
+          title="You have rejected the invite"
+          description="You can no longer create your account; Contact the institute owner"
+        />
+      ) : (
+        // !expired accepted used
+        <Card
+          title="You have already created your account"
+          description="You can no longer create your account; Contact the institute owner"
+        />
+      )}
+    </>
+  );
+
+  return (
+    <div className="grid place-content-center min-h-screen">
+      {!expired ? (
+        <>{!used ? <>{notUsedComponent}</> : <>{acceptedComponent}</>}</>
+      ) : (
+        <Card
+          title="Invite Expired"
+          description="You can no longer create your account; Contact the institute owner"
+        />
+      )}
+    </div>
+  );
 }
